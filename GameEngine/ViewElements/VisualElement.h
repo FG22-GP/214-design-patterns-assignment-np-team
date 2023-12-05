6abb2cd3cd9310178f2b3aab01e5913bebc9f12a
Nestorboy <35258953+Nestorboy@users.noreply.github.com>
2023-12-05 17:10:18 +0100

    Added some fields to VisualElements

--- GameEngine/ViewElements/VisualElement.h
+++ GameEngine/ViewElements/VisualElement.h
@@ -7,5 +7,10 @@ struct VisualElement
     //List of Renderables?
     //Maybe a type description?
     
-    
+    SDL_Rect Rect;
+    bool Selectable;
+
+    // Style could be isolated
+    SDL_Texture* Texture;
+    SDL_Color Color;
 };
